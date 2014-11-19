4b90fa0e03ca418264ef7162a62d00af32e14680
Gabriel Berlanga <g.berlanga.s@gmail.com>
2014-11-18 23:54:18 -0600

    nombres

--- app/models/order_vehicle_workshop.rb
+++ app/models/order_vehicle_workshop.rb
@@ -2,4 +2,8 @@ class OrderVehicleWorkshop < ActiveRecord::Base
   belongs_to :order, inverse_of: :order_vehicle_workshops
   belongs_to :vehicle, inverse_of: :order_vehicle_workshops
   belongs_to :workshop, inverse_of: :order_vehicle_workshops
+
+  validates :vehicle_id, presence: true
+  validates :workshop_id, presence: true
+  validates :order_id, presence: true
 end
