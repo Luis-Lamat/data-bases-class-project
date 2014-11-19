8917c7559743d3a52a029dbbfc72d17595bf3687
Gabriel Berlanga <g.berlanga.s@gmail.com>
2014-11-18 22:18:33 -0600

    validates todo excepto las relaciones

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,6 @@
 class Location < ActiveRecord::Base
+	validates :street, presence: true
+	validates :street_number, presence: true
+	validates :suburb, presence: true
+	validates :postal_code, presence: true
 end
--- app/models/spare_part.rb
+++ app/models/spare_part.rb
@@ -1,2 +1,6 @@
 class SparePart < ActiveRecord::Base
+	validates :name, presence: true
+	validates :provider, presence: true
+	validates :cost, presence: true
+	validates :quantity, presence: true
 end
