2c5be2b8566d7edaa5210aae650f8993d26f13d0
Luis-Lamat <beto_alaska@hotmail.com>
2014-11-25 21:54:30 -0600

    Fixes conflict

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,10 +3,6 @@ class Client < ActiveRecord::Base
   has_many :vehicles, inverse_of: :client
   belongs_to :location, inverse_of: :client
 
-<<<<<<< HEAD
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-=======
 	validates :first_name, presence: true
 	validates :last_name, presence: true
 
@@ -14,5 +10,4 @@ class Client < ActiveRecord::Base
     first_name + " " + last_name + " - Vehiculos: " + vehicles.count.to_s 
   end
 
->>>>>>> b806658b78b30bc306ccdf3f4ac455494f7e4012
 end
